0ac5590397066e818dbd7fb468ded583590c60d6
Sandeep <119731624+sandeep-sunkara@users.noreply.github.com>
2024-07-29 17:53:30 +0530

    Update atm_project.py

--- atm_project.py
+++ atm_project.py
@@ -2,45 +2,40 @@ import authentication
 import tasks
 
 print("//WELCOME TO SA ATM//")
-a=input("Enter username:")
-b=input("Enter password:")
+username = input("Enter username: ")
+password = input("Enter password: ")
 
-aobj=authentication.auth(a,b)
-# print(aobj.autentication())
-if (aobj.autentication()):
-    tobj=tasks.balance()
-    tobj.bal()
-    print("enter card type:")
-    (print("1.rupay""\n""2.visa""\n""3.mastercard"))
-    ct=input()
-    if (tobj.cards(ct)):
+auth_obj = authentication.Auth(username, password)
+
+if auth_obj.authenticate():
+    task_obj = tasks.Balance()
+    task_obj.display_atm_balance()
+    print("Enter card type:")
+    print("1. Rupay\n2. Visa\n3. Mastercard")
+    card_type = input()
+    
+    if task_obj.authenticate_card(card_type):
         while True:
             print("Menu:")
-            (print("1.checkbalance""\n""2.withdrawl""\n""3.deposit""\n""4.ministatement""\n""5.exit"))
-            task=input()
-            list1=[]
-            if(task=="1"or task=="checkbalance"):
-                rt=tobj.cb()
+            print("1. Check Balance\n2. Withdraw\n3. Deposit\n4. Mini Statement\n5. Exit")
+            task = input()
+
+            if task == "1" or task.lower() == "check balance":
+                task_obj.check_balance()
                 print()
-                # list1.append(rt)
-            elif(task=="2"or task=="withdrawl"):
-                wda=int(input("Enter amount to withdraw:"))
-                rt=tobj.wd(wda)
-                # list1.append(rt)
-            elif(task=="3"or task=="deposit"):
-                da=int(input("enter amount to deposit:"))
-                rt=tobj.dp(da)
+            elif task == "2" or task.lower() == "withdraw":
+                withdrawal_amount = int(input("Enter amount to withdraw: "))
+                task_obj.withdraw(withdrawal_amount)
+            elif task == "3" or task.lower() == "deposit":
+                deposit_amount = int(input("Enter amount to deposit: "))
+                task_obj.deposit(deposit_amount)
                 print()
-                # list1.append(rt)
-            elif(task=="4"or task=="ministatement"):
-                #print("list1[-3:]")
-                print("CURRENTLY UNAVALIABLE")
+            elif task == "4" or task.lower() == "mini statement":
+                print("CURRENTLY UNAVAILABLE")
                 print()
-            elif(task=="5"or task=="exit"):
-                print("THANK FOR USING SA ATM")
+            elif task == "5" or task.lower() == "exit":
+                print("THANK YOU FOR USING SA ATM")
                 break
             else:
-                print("invalid option")
-                print()        
-
-    
+                print("Invalid option")
+                print()
